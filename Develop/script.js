7a15561560f416b01aad972060c414258e4d8c39
AKim47 <abel.kim.22@gmail.com>
2021-10-10 18:28:40 -0500

    added generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -4,12 +4,21 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+
+}
+
+function generatePassword() {
+// criteria:
+var charLength = window.prompt("Choose a paswword length between 8-128 characters");
+while (charLength > 128 || charLength < 8) {
+  var charLength = window.prompt("Please choose a valid number between 8 and 128");
+};
 
 }
 
