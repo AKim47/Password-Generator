2dfcb59fbd530f65814f9695ddcd1cc3d8c17226
AKim47 <abel.kim.22@gmail.com>
2021-10-10 18:39:29 -0500

    added password criteria to generatePassword

--- Develop/script.js
+++ Develop/script.js
@@ -15,12 +15,24 @@ function writePassword() {
 
 function generatePassword() {
 // criteria:
+
+// desired character length
 var charLength = window.prompt("Choose a paswword length between 8-128 characters");
 while (charLength > 128 || charLength < 8) {
   var charLength = window.prompt("Please choose a valid number between 8 and 128");
 };
 
+// lowercase Y/N
+var lowerCase = window.confirm("Press OK to include lowercase in your password or Press CANCEL to not include lowercase in your password.");
 
+// uppercase Y/N
+var upperCase = window.confirm("Press OK to include uppercase in your password or Press CANCEL to not include uppercase in your password.");
+
+// numeric Y/N
+var numeric = window.confirm("Press OK to include numbers in your password or Press CANCEL to not include numbers in your password.");
+
+// special character Y/N
+var special = window.confirm("Press OK to include uppercase in your password or Press CANCEL to not include uppercase in your password.");
 }
 
 // Add event listener to generate button
